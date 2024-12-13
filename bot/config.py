0d98b8ab6847623f9f6bf042acfd799703db4d76
Pavan844 <117534484+Pavan844@users.noreply.github.com>
2024-12-13 17:54:21 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,30 +5,30 @@ load_dotenv()
 
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 20516707))
+    API_ID = int(env.get("26009823", 1234))
+    API_HASH = env.get("e545fc56028ee9404ef5b5bec64503ca", "xyz")
+    OWNER_ID = int(env.get("OWNER_ID", 6059507751))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "InstantLinkELBot")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001979167499))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "Kannadamovies844bot")
+    BOT_TOKEN = env.get("6880540335:AAGIPoc1_TPahTKxUHAoRhHB5Rm328cNtwE", "1234:abcd")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002197394981))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("BASE_URL", "https://filestreambot-2jts.onrender.com")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
 
 class DB:
-    DB_URL = env.get("DB_URL", "postgresql://uname:pass@locahost:5432/dbname")
+    DB_URL = env.get("DB_URL", "mongodb+srv://pkmoviesdb:C2C4VqSwGdigpbjQ@cluster0.7rk54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 
 class Util:
     PING_INTERVAL = int(env.get("PING_INTERVAL ", 1200))  # 20 minutes
     SUB_CHANNEL = int(env.get("SUB_CHANNEL", 0))
-    SUB_CHANNEL_LINK = env.get("SUB_CHANNEL_LINK", "t.me/elupdates")
+    SUB_CHANNEL_LINK = env.get("SUB_CHANNEL_LINK", "t.me/pkmovies4u")
 
 
 # LOGGING CONFIGURATION
